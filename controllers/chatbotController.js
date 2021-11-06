15c1bc6631fb0361ea1ff29cec7976b3dfe72ccd
hoanganhquang <anhquang976@gmail.com>
2021-11-06 17:11:23 +0700

    update

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -1,6 +1,8 @@
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 exports.getWebHook = (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
   let mode = req.query["hub.mode"];
@@ -28,10 +30,15 @@ exports.postWebHook = (req, res) => {
   if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      body.entry.forEach(function (entry) {
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log("Sender PSID: " + sender_psid);
+      });
     });
 
     // Returns a '200 OK' response to all requests
@@ -41,3 +48,12 @@ exports.postWebHook = (req, res) => {
     res.sendStatus(404);
   }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
