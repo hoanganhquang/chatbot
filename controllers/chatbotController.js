c5864d28548155b891da7e5bc0d0260a9c358e89
hoanganhquang <anhquang976@gmail.com>
2021-11-06 17:39:52 +0700

    update

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -65,7 +65,37 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message
     response = {
-      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+      text: `Chào bạn`,
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "ảnh bạn à?",
+              subtitle: "Nhấn để trả lời",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Đúng!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "Không!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
     };
   }
 
@@ -74,7 +104,21 @@ function handleMessage(sender_psid, received_message) {
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {}
+function handlePostback(sender_psid, received_postback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
